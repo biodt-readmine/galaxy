5c491cb5f1bbc18853adbd3443d2c6ad3bf6528f
Dmitry Nosov <test@biodt.org>
2013-05-20 14:29:53 +0700

    Initial data commit to check jUnit and static analysis.

---
+++ src/main/java/y/App.java
@@ -0,0 +1,198 @@
+package y;
+
+/**
+ * Hello world!
+ *
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+ 
+ public class App 
+{
+    public static void main( String[] args )
+    {
+		int a = 5;
+		int b = 6;
+		int c = 6;
+		int d = 6;
+		int e = 6;		
+		System.out.println( "Hello World!" );
+		a = 5;
+		b = 6;
+		c = 6;
+		d = 6;
+		e = 6;		
+		System.out.println( "Hello World!" );
+		a = 5;
+		b = 6;
+		c = 6;
+		d = 6;
+		e = 6;
+    }
+	
+	public static void method0() {
+				
+	}
+	
+	public static void method1() {
+		System.out.println( "Hello World!" );				
+	}
+
+	public static void method2() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method3() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method4() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method5() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method6() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method7() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method8() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method9() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method10() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method11() {
+		System.out.println( "Hello World!" );				
+	}
+
+	public static void method12() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method13() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method14() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method15() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method16() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method17() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method18() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method19() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method20() {
+		System.out.println( "Hello World!" );				
+	}
+	public static void method21() {
+		System.out.println( "Hello World!" );				
+	}
+
+	public static void method22() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method23() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method24() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method25() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method26() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method27() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method28() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method29() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method30() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method31() {
+		System.out.println( "Hello World!" );				
+	}
+
+	public static void method32() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method33() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method34() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method35() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method36() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method37() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method38() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method39() {
+		System.out.println( "Hello World!" );				
+	}
+	
+	public static void method40() {
+		System.out.println( "Hello World!" );				
+	}	
+	
+}
