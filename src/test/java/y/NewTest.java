cf57b221010bb93ff1f52f1b37c15c9a1ad81c1e
Anotrer Ferma <developeranother2@gmail.com>
2013-06-07 14:29:54 +0700

    Changes to pom and tests

--- src/test/java/y/NewTest.java
+++ src/test/java/y/NewTest.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+
+
 @Category(IntegrationTest.class)
-public class NewTest  {
-        public void testBrowser() {
+public class NewTest {
+
+	@Test
+	@Category(y.IntegrationTest.class)
+	public void emptyTest1() throws Exception {
         HtmlUnitDriver driver = new HtmlUnitDriver();
         driver.get("http://www.google.com/webhp?complete=1&hl=en");
         WebElement query = driver.findElement(By.name("q"));
-        }
+	}
 }
